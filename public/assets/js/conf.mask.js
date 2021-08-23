3959e3f12a5fec443f53ccaa0eb69fa7d9fdf12b
alisson fredo <douglasalissonsf@gmail.com>
2021-08-23 14:32:40 -0300

    adicionando mensagem de feedback para o usuario

---
+++ public/assets/js/conf.mask.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    "use strict";
+
+    /* EXEMPLOS
+        $("#cnpj").mask("000.000.000-00", { reverse: true });
+        $(".telefone").mask("(00) 00000-0000");
+        $(".cep").mask("00000-000");
+    */
+});
